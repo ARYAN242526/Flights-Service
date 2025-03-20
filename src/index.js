c69fe317dd774b6eecc9cb5bc246c4d809a62893
ARYAN242526 <roy2503aryan@gmail.com>
2025-03-20 20:28:12 +0530

    made some changes in src index.js file

--- src/index.js
+++ src/index.js
@@ -20,32 +20,6 @@ app.use('/api' , apiRoutes);
 
 app.listen(PORT , async() => {
     console.log(`Server is listening on PORT : ${PORT} `);
-    // bad code alert 
-   // const bengaluru = await City.findByPk(1);
-   // console.log(bengaluru);
-    // const airport = await Airport.create({name : 'kempegowda airport' , code : 'BLR' , cityId : 1});
-    //  const hbairport = await bengaluru.createAirport({name : 'Hubbali airport' , code : 'HBL'})
-    //  console.log(hbairport);
-    // const airportsInBlr = await bengaluru.getAirports();
-    // console.log(airportsInBlr);
-    // const hbairport = await Airport.findByPk(3);
-    // console.log(hbairport);
-    // await bengaluru.removeAirport(hbairport);
-
-    // await City.destroy({
-    //     where : {
-    //         id : 1
-    //     }
-    // })
-
-    //  const city = await City.findByPk(3);
-    // // console.log(city);
-    // await city.createAirport({name : "Indore Airport" , code : 'IND'})
-
-    await City.destroy({
-        where : {
-            id : 3
-        }
-    })
+  
      
 });
